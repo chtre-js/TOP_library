57eef0d4289dfa0e173ce5a7906d07da1177c97f
Cherif <227361371+chtre-js@users.noreply.github.com>
2026-07-28 14:37:50 +0200

    feat(library): add book data model and collection

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// Initial commit
+const myLibrary = [];
+
+function Book(id, title, author, pages, readStatus) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+};
+
+function addBook(title, author, pages, readStatus) {
+    const id = crypto.randomUUID();
+    const newBook = new Book(id, title, author, pages, readStatus);
+
+    myLibrary.push(newBook);
+}
